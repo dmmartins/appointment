7dd293020a02ce062badf1cb1cc46f7329e7a6ba
Diego Manenti Martins <dmmartins@gmail.com>
2010-06-01 23:38:05 -0300

    required login to create appointment

--- appointment.py
+++ appointment.py
@@ -5,9 +5,10 @@
 
 import os
 import datetime
-import gettext
+import functools
 
 # GAE imports
+from google.appengine.api import users
 from google.appengine.api import mail
 from google.appengine.ext import webapp
 from google.appengine.ext import db
@@ -39,6 +40,10 @@ class Appointment(db.Model):
     name = db.StringProperty(required=True)
     email = db.EmailProperty(required=True)
 
+    @property
+    def dates(self):
+        return ', '.join([d.strftime(settings.DATETIME_FORMAT) for d in self.date_list])
+
 
 class Invite(db.Model):
     ''' Contact invited to an appointment. '''
@@ -48,13 +53,37 @@ class Invite(db.Model):
     appointment = db.ReferenceProperty(Appointment)
 
 
+# Login required decorator
+def login_required(method, admin=False):
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        user = users.get_current_user()
+        if not user:
+            if self.request.method == 'GET':
+                self.redirect(users.create_login_url(self.request.uri))
+                return
+            self.error(403)
+        elif admin and not users.is_current_user_admin():
+            self.error(403)
+        else:
+            return method(self, *args, **kwargs)
+    return wrapper
+
+# Administrator required decorator
+def admin_required(method):
+    return login_required(method, admin=True)
+
 class BaseRequestHandler(webapp.RequestHandler):
     ''' Suplies a common template generation method. '''
     def generate(self, template_name, template_values={}):
         lang = self.request.get('lang')
         translation.activate(lang)
+
         values = {
             'request': self.request,
+            'user': self.current_user,
+            'login_url': users.create_login_url(self.request.uri),
+            'logout_url': users.create_logout_url('http://%s/' % self.request.host),
             'settings': settings,
         }
 
@@ -67,8 +96,24 @@ class BaseRequestHandler(webapp.RequestHandler):
         super(BaseRequestHandler, self).error(code)
         self.response.out.write(self.response.http_status_message(code))
 
+    @property
+    def current_user(self):
+        user = users.get_current_user()
+        if user:
+            user.administrator = users.is_current_user_admin()
+        return user
+
 
 class HomeHandler(BaseRequestHandler):
+    def get(self):
+        if self.current_user:
+            self.redirect('/new')
+        else:
+            self.generate('index.html')
+
+
+class NewAppointmentHandler(BaseRequestHandler):
+    @login_required
     def get(self):
         key = self.request.get('key')
         if key:
@@ -76,8 +121,9 @@ class HomeHandler(BaseRequestHandler):
         else:
             appointment = None
 
-        self.generate('index.html', {'appointment': appointment})
+        self.generate('new.html', {'appointment': appointment})
 
+    @login_required
     def post(self):
         description = self.request.get('description')
         invitees = self.request.get('invitees')
@@ -99,19 +145,6 @@ class HomeHandler(BaseRequestHandler):
             )
         appointment.put()
 
-        # Send a confirmation email
-        msg = mail.EmailMessage()
-        msg.subject = 'Confirmation: %s' % appointment.description
-        msg.sender = settings.app_config['email']
-        msg.to = appointment.email
-        directory = os.path.dirname(__file__)
-        path = os.path.join(directory, 'template', 'confirmation.html')
-        msg.html = template.render(path, {
-            'appointment': appointment,
-            'settings': settings,
-            'request': self.request})
-        msg.send()
-
         # Save invitees
         for date in date_list:
             owner = Invite(
@@ -131,8 +164,35 @@ class HomeHandler(BaseRequestHandler):
                     )
                 i.put()
 
-        #self.redirect('/appointment?key=%s&user=%s' % (appointment.key(), appointment.email))
-        self.generate('created.html')
+        # Send the overview e-mail
+        msg = mail.EmailMessage()
+        msg.subject = 'Overview: %s' % appointment.description
+        msg.sender = settings.app_config['email']
+        msg.to = appointment.email
+        directory = os.path.dirname(__file__)
+        path = os.path.join(directory, 'template', 'overview.html')
+        msg.html = template.render(path, {
+            'appointment': appointment,
+            'settings': settings,
+            'request': self.request})
+        msg.send()
+
+        # Send an email invitation for each invitee
+        for invitee in appointment.invitee_list:
+            msg = mail.EmailMessage()
+            msg.subject = 'Invitation: %s' % appointment.description
+            msg.sender = settings.app_config['email']
+            msg.to = invitee
+            directory = os.path.dirname(__file__)
+            path = os.path.join(directory, 'template', 'invitation.html')
+            msg.html = template.render(path, {
+                'appointment': appointment,
+                'invitee': invitee,
+                'settings': settings,
+                'request': self.request})
+            msg.send()
+
+        self.redirect('/appointment?key=%s&user=%s' % (appointment.key(), appointment.email))
 
 
 class AppointmentHandler(BaseRequestHandler):
@@ -147,6 +207,7 @@ class AppointmentHandler(BaseRequestHandler):
 
 
 class AppointmentsHandler(BaseRequestHandler):
+    @admin_required
     def get(self):
         appointments = Appointment.all().order('-date_list')
         self.generate('appointment_list.html', {'appointments': appointments})
@@ -187,52 +248,19 @@ class AvailabilityHandler(BaseRequestHandler):
         invitee.put()
 
 
-class ConfirmHandler(BaseRequestHandler):
-    ''' Confirm the appointment and send email invitation to all invitees. '''
+class Http404(BaseRequestHandler):
     def get(self):
-        key = self.request.get('key')
-        appointment = Appointment.get(key)
-        if not appointment:
-            return self.error(404)
-
-        # Send the overview e-mail
-        msg = mail.EmailMessage()
-        msg.subject = 'Overview: %s' % appointment.description
-        msg.sender = settings.app_config['email']
-        msg.to = appointment.email
-        directory = os.path.dirname(__file__)
-        path = os.path.join(directory, 'template', 'overview.html')
-        msg.html = template.render(path, {
-            'appointment': appointment,
-            'settings': settings,
-            'request': self.request})
-        msg.send()
-
-        # Send an email invitation for each invitee
-        for invitee in appointment.invitee_list:
-            msg = mail.EmailMessage()
-            msg.subject = 'Invitation: %s' % appointment.description
-            msg.sender = settings.app_config['email']
-            msg.to = invitee
-            directory = os.path.dirname(__file__)
-            path = os.path.join(directory, 'template', 'invitation.html')
-            msg.html = template.render(path, {
-                'appointment': appointment,
-                'invitee': invitee,
-                'settings': settings,
-                'request': self.request})
-            msg.send()
-
-        self.redirect('/appointment?key=%s&user=%s' % (appointment.key(), appointment.email))
+        return self.error(404)
 
 
 if __name__ == '__main__':
     application = webapp.WSGIApplication([
         (r'/', HomeHandler),
+        (r'/new', NewAppointmentHandler),
         (r'/appointment', AppointmentHandler),
         (r'/appointments', AppointmentsHandler),
         (r'/availability', AvailabilityHandler),
-        (r'/confirm', ConfirmHandler),
+        (r'/.*', Http404),
         ], debug=_DEBUG)
     run_wsgi_app(application)
 
